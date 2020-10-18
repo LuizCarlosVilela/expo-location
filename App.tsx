6d24757dc7e541c4001992c5e149ad576461aed6
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-10-18 20:53:38 -0300

    iniciando projeto

--- App.tsx
+++ App.tsx
@@ -1,12 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import * as Location from "expo-location";
+
+interface localizacao {
+  latitude: number;
+  longitude: number;
+  altitude: number | null;
+  accuracy: number | null;
+  altitudeAccuracy: number | null;
+  heading: number | null;
+  speed: number | null;
+}
 
 export default function App() {
+  const [location, setLocation] = useState<localizacao>();
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      setStatus(status);
+    })();
+  }, []);
+
+  const getLocation = async () => {
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+    }
+
+    const data: Location.LocationObject = await Location.getCurrentPositionAsync(
+      {}
+    );
+    const { coords } = data;
+    setLocation(coords);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{location?.latitude}</Text>
+      <Text>{location?.longitude}</Text>
+      <TouchableOpacity
+        onPress={() => getLocation()}
+        style={{
+          padding: 20,
+          backgroundColor: "blue",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Text>Toque aqui</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +59,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
   },
 });
